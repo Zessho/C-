678913b1d48e1c8cd157a193c14121ece262f81a
Zessho <124446033+Zessho@users.noreply.github.com>
2024-01-25 18:29:07 +0300

    Update Задание 2.cs

--- Задание 2/Задание 2.cs
+++ Задание 2/Задание 2.cs
@@ -1,18 +1,14 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Введите число 1:");
-int num1 = Convert.ToInt32(Console.ReadLine());
-Console.WriteLine("Введите число 2:");
-int num2 = Convert.ToInt32(Console.ReadLine());
 
-if (num1 > num2)
+Console.WriteLine("Введите число:");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num > 5 && num < 10) 
 {
-    Console.WriteLine($"Число {num1} больше числа {num2}");
+    Console.WriteLine("Число больше 5 и меньше 10");
 }
-else if (num1 < num2)
-{
-    Console.WriteLine($"Число {num1} меньше числа {num2}");
-}
+
 else
 {
-    Console.WriteLine($"Число {num1} равно числу {num2}");
+    Console.WriteLine("Неизвестное число");
 }
